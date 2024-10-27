8d576ce19a5a14ba4e13b2cf66d07971b4b8f93c
Benhuang0857 <benhuang0857@gmail.com>
2024-10-27 10:37:50 +0800

    Add gRPC

---
+++ WebApplication1/Services/XSCADService.cs
@@ -0,0 +1,13 @@
+﻿using Grpc.AspNetCore;
+using Grpc.Core;
+
+namespace WebApplication1.Services
+{
+    public class XSCADService
+    {
+        public override Task<XSCADResponse> XSCADProto(XSCADRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new XSCADResponse { Message = $"Hello, {request.Name}!" });
+        }
+    }
+}
